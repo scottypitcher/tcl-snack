5cb428cdc933d75c31da441d431cf4ffd3572fa3
Scott Pitcher <scottypitcher@gmail.com>
2021-11-07 09:31:15 +1100

    SVP 7NOV2021: Corrected +x/-x permissions

--- unix/sver.sh
+++ unix/sver.sh
@@ -6,20 +6,20 @@ else
     KEY=SNACK_VERSION
 fi
 RESULT=`grep $KEY "$1"`
-# echo "RESULT=$RESULT"
+echo "RESULT=$RESULT"
 THIS_ONE=0
 for i in $RESULT; do
     if test "$THIS_ONE" = "1" ; then
-	# echo "THIS ONE is \'$i\'"
+	echo "THIS ONE is $i"
 	LEN=`expr length "$i"`
-	# echo "LEN is $LEN"
-	END=`expr $LEN - 2`
-	# echo "END is $END"
+	echo "LEN is $LEN"
+	END=`expr $LEN - 3`
+	echo "END is $END"
 	VERSION=`expr substr "$i" 2 $END`
     fi
     if test "$i" = "$KEY"; then
 	THIS_ONE=1
     fi
 done
-# echo "final VERSION is '$VERSION'"
+echo "final VERSION is '$VERSION'"
 echo $VERSION
